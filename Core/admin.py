0d95766c61dfd9652cead194d86f83954643e19f
Nima Bayat <bayatnima343@gmail.com>
2025-01-09 10:26:18 -0500

    core app completed.

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Image
 
 # Register your models here.
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+
+    list_display = ('name' , 'Image', 'is_default', 'create_at')
+    list_filter = ('is_default',)
+    search_fields = ('name',)
--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from django.db import models 
+from django.utils.translation import gettext_lazy as _
+import uuid
+
+
 
 # Create your models here.
+
+class BaseModel(models.Model):
+
+    id = models.UUIDField(primary_key=True , default=uuid.uuid4 , editable= False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        abstract = True
+
+
+    
+
+class Image(BaseModel):
+
+    name = models.CharField(_('Name'), max_length=200)
+    alt_text = models.CharField(_('Alternative Text'), max_length=50 , blank=True)
+    model_name = models.CharField(max_length=50)
+    Image = models.ImageField(_('Image'), upload_to='images/%(model_name)s/')
+    is_default = models.BooleanField(default=False)
+
+
+    class Meta:
+
+        verbose_name = _('Image')
+        verbose_name_plural = _('Images')
