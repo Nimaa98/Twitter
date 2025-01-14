e26d0386656b4214a3b0f7055a75d9478dd1c6c6
Nima Bayat <bayatnima343@gmail.com>
2025-01-14 11:28:18 -0500

    Content Admin added.

--- Contents/admin.py
+++ Contents/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Post , Tag , Comment , Like
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    
+    list_display = ('title' ,'user','create_at')
+    search_fields = ('tags',)
+    list_filter = ('user',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
+    list_display = ('post' ,'user','parent','create_at')
+    search_fields = ('post',)
+    list_filter = ('parent',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    
+    list_display = ('text' ,'slug','create_at')
+    search_fields = ('text',)
+    list_filter = ('slug',)
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    
+    list_display = ('post' ,'user','create_at')
+    list_filter = ('post',)
